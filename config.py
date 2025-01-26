7d194adb0de023ddc8e448a69f3805daa1a59ada
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2025-01-25 17:20:57 -0800

    Remove hardcoded API key from config

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_KEY = "fw_3ZjtsywUGddwa1wGY4VvB3eW"
+API_KEY = ""
 URL = "https://api.fireworks.ai/inference/v1/chat/completions"
 MODEL = "accounts/sentientfoundation/models/dobby-mini-unhinged-llama-3-1-8b#accounts/sentientfoundation/deployments/81e155fc"
